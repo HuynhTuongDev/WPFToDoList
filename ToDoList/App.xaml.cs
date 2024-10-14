05ece0ab40d5b7047966a3255a064a2949489cf7
lehuynhtuong <lehuynhtuong13@gmail.com>
2024-10-14 16:52:55 +0700

    MainWindow

--- ToDoList/App.xaml.cs
+++ ToDoList/App.xaml.cs
@@ -1,14 +1,56 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using DataAccessLayer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Repositories;
+using ToDoList.Services;
 using System.Windows;
 
 namespace ToDoList
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        public static ServiceProvider ServiceProvider { get; private set; }
+        public IConfiguration Configuration { get; private set; }
 
+        private void ConfigureServices(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile(@"D:\Ki5\ToDoList\ToDoList\appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            services.AddSingleton(Configuration);
+
+            // Configure DbContext via DI
+            services.AddDbContext<TaskManagementContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Register repositories, services, etc.
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<ITodoRepository, TodoRepository>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<ITodoService, TodoService>();
+
+            services.AddTransient<LoginWindow>();
+            services.AddTransient<RegisterWindow>();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Configure ServiceCollection
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            // Build ServiceProvider
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            // Open login window
+            var loginWindow = ServiceProvider.GetRequiredService<LoginWindow>();
+            loginWindow.Show();
+        }
+    }
 }
---
+++ ToDoList/User/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using BusinessObject;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public User User { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Xử lý khi văn bản trong ô tìm kiếm thay đổi
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Xử lý khi nút tìm kiếm được nhấn
+        }
+
+        private void AddTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Xử lý khi nút thêm công việc được nhấn
+        }
+
+        private void EditTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Xử lý khi nút chỉnh sửa công việc được nhấn
+        }
+
+        private void DeleteTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Xử lý khi nút xóa công việc được nhấn
+        }
+    }
+
+}
