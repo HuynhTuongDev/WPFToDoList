a4966b39aa5c9de72841ba1dafed604066ac525a
DHGiakhanh <khanhdhgqe180127@fpt.edu.vn>
2024-10-22 12:57:32 +0700

    UserManage

--- ToDoList/User/MainWindow.xaml.cs
+++ ToDoList/User/MainWindow.xaml.cs
@@ -1,49 +1,108 @@
 ﻿using BusinessObject;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Linq;
 
 namespace ToDoList
 {
     public partial class MainWindow : Window
     {
         public User User { get; set; }
+        public ObservableCollection<Todo> Todos { get; set; }
+        public ObservableCollection<Todo> DoneTodos { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
+            Todos = new ObservableCollection<Todo>();
+            DoneTodos = new ObservableCollection<Todo>();
+            UserTasksListView.ItemsSource = Todos;
         }
 
         private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            // Xử lý khi văn bản trong ô tìm kiếm thay đổi
+            string keyword = SearchTextBox.Text.ToLower();
+
+            var filteredTodos = Todos.Where(todo =>
+                todo.Title.ToLower().Contains(keyword) ||
+                todo.Description.ToLower().Contains(keyword)) 
+                .OrderBy(todo => todo.IsCompleted)
+                .ThenBy(todo => todo.DueDate)
+                .ToList();
+
+            UserTasksListView.ItemsSource = filteredTodos;
         }
 
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
-            // Xử lý khi nút tìm kiếm được nhấn
+            string keyword = SearchTextBox.Text.ToLower();
+
+            var filteredTodos = Todos.Where(todo =>
+                todo.Title.ToLower().Contains(keyword) ||
+                todo.Description.ToLower().Contains(keyword))
+                .OrderBy(todo => todo.IsCompleted)
+                .ThenBy(todo => todo.DueDate)
+                .ToList();
+
+            UserTasksListView.ItemsSource = filteredTodos;
         }
 
         private void AddTaskButton_Click(object sender, RoutedEventArgs e)
         {
-            // Xử lý khi nút thêm công việc được nhấn
-            CreateTask createTask = new CreateTask();
-            createTask.ShowDialog();
+            CreateTask createTaskWindow = new CreateTask();
+            if (createTaskWindow.ShowDialog() == true)
+            {
+                var newTodo = createTaskWindow.NewTodo;
+
+                Todos.Add(newTodo);
+            }
         }
 
         private void EditTaskButton_Click(object sender, RoutedEventArgs e)
         {
-            // Xử lý khi nút chỉnh sửa công việc được nhấn
-            UpdateTask updateTask = new UpdateTask();
-            updateTask.ShowDialog();
+            var selectedTodo = (Todo)UserTasksListView.SelectedItem;
+
+            if (selectedTodo != null)
+            {
+                UpdateTask updateTask = new UpdateTask(selectedTodo);
+                if (updateTask.ShowDialog() == true)
+                {
+                    UserTasksListView.Items.Refresh();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một công việc để chỉnh sửa.", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
 
         private void DeleteTaskButton_Click(object sender, RoutedEventArgs e)
         {
-            // Xử lý khi nút xóa công việc được nhấn
-            DeleteTask deletetask = new DeleteTask();
-            deletetask.ShowDialog();
+            var selectedTodo = (Todo)UserTasksListView.SelectedItem;
+
+            if (selectedTodo != null)
+            {
+                DeleteTask deleteTask = new DeleteTask(() =>
+                {
+                    Todos.Remove(selectedTodo);
+                    MessageBox.Show($"Công việc '{selectedTodo.Title}' đã được xóa.", "Xóa Thành Công");
+                });
+
+                deleteTask.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một công việc để xóa.", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
+
+        private void UserTasksListView_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            UserTasksListView.ItemsSource = Todos.OrderBy(todo => todo.IsCompleted).ThenBy(todo => todo.DueDate).ToList();
+        }
+
 
         private void LogoutButton_Click(object sender, RoutedEventArgs e)
         {
